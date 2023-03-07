536e84fb0924f61b0445fef4e8c89b3a79832de0
lizelx <lizalesa@gmail.com>
2023-03-07 17:00:45 +0300

    Добавлены в класс SQLiteRepository методы (заготовки) add (из конспекта лекции), get, get_all, update и delete.

--- bookkeeper/repository/sqlite_repository.py
+++ bookkeeper/repository/sqlite_repository.py
@@ -9,6 +9,7 @@ class SQLiteRepository(AbstractRepository[T]):
     def __init__(self, db_file: str, cls: type) -> None:
         self.db_file = db_file
         self.table_name = cls.__name__.lower()
+
         self.fields = get_annotations(cls, eval_str=True)
         self.fields.pop('pk')
 
