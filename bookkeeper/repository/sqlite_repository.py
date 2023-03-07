c2cbcf86cd5eeb587a2ecb86bac6e699edda52f0
lizelx <lizalesa@gmail.com>
2023-03-07 16:57:06 +0300

    Добавлены в прототип класса SQLiteRepository методы (заготовки) add (из конспекта лекции), get, get_all, update и delete.

--- bookkeeper/repository/sqlite_repository.py
+++ bookkeeper/repository/sqlite_repository.py
@@ -1,17 +1,52 @@
+import sqlite3
+from inspect import get_annotations
+from bookkeeper.repository.abstract_repository import AbstractRepository, T
 from dataclasses import dataclass
 from datetime import datetime
 from inspect import get_annotations
 
-
-@dataclass
-class Test:
-    name: str
-    pk: int = 0
-
-class SQLiteRepository:
-    def __init__(self, db_file:str, cls: type) -> None:
+class SQLiteRepository(AbstractRepository[T]):
+    def __init__(self, db_file: str, cls: type) -> None:
         self.db_file = db_file
         self.table_name = cls.__name__.lower()
-        self.fields = get_annotations(cls, eval_str = True)
+        self.fields = get_annotations(cls, eval_str=True)
         self.fields.pop('pk')
 
+    def add(self, obj: T) -> int:
+        names = ', '.join(self.fields.keys())
+        p = ', '.join("?" * len(self.fields))
+        values = [getattr(obj, x) for x in self.fields]
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute('PRAGMA foreign_keys = ON')
+            cur.execute(
+                f'INSERT INTO {self.table_name} ({names}) VALUES ({p})', values
+            )
+            obj.pk = cur.lastrowid
+        con.close()
+        return obj.pk
+
+    def get(self, pk: int) -> T | None:
+        """ Получить объект по id """
+        pass
+
+    def get_all(self, where: dict[str, any] | None = None) -> list[T]:
+        """
+        Получить все записи по некоторому условию
+        where - условие в виде словаря {'название_поля': значение}
+        если условие не задано (по умолчанию), вернуть все записи
+        """
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute(f'SELECT * FROM {self.table_name}')  #
+            res = cur.fetchall()
+        con.close()
+        return res
+
+    def update(self, obj: T) -> None:
+        """ Обновить данные об объекте. Объект должен содержать поле pk. """
+        pass
+
+    def delete(self, pk: int) -> None:
+        """ Удалить запись """
+        pass
