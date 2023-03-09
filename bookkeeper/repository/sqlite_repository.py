883f7248d1baad4c8167687eec4350e8ff91c346
lizelx <lizalesa@gmail.com>
2023-03-09 21:53:16 +0300

    Исправлены ошибки в методах update и get

--- bookkeeper/repository/sqlite_repository.py
+++ bookkeeper/repository/sqlite_repository.py
@@ -1,13 +1,17 @@
 import sqlite3
-from inspect import get_annotations
 from bookkeeper.repository.abstract_repository import AbstractRepository, T
 from typing import Any
-from dataclasses import dataclass
-from datetime import datetime
 from inspect import get_annotations
 
 
-
+def obj_make(cls: Any, fields: dict[Any, Any], values: str) -> Any:
+    res = object.__new__(cls)
+    if values is None:
+        return None
+    for attr, val in zip(fields.keys(), values):
+        setattr(res, attr, val)
+    setattr(res, 'pk', values[-1])
+    return res
 
 
 class SQLiteRepository(AbstractRepository[T]):
@@ -15,62 +19,42 @@ class SQLiteRepository(AbstractRepository[T]):
         self.cls: type = cls
         self.db_file = db_file
         self.table_name = cls.__name__.lower()
-
         self.fields = get_annotations(cls, eval_str=True)
-        names = ', '.join(self.fields.keys())
         self.fields.pop('pk')
+        self.conn = sqlite3.connect(self.db_file)
+        self.cursor = self.conn.cursor()
         with sqlite3.connect(self.db_file) as con:
             cur = con.cursor()
-            cur.execute(f'CREATE TABLE IF NOT EXISTS {self.table_name} ({names})')
-            print(self.fields)
-            #con.close()
-
+            fileds_type = ' '.join([f"{field} TEXT," for field in self.fields])
+            cur.execute(f'CREATE TABLE IF NOT EXISTS {self.table_name} ({fileds_type}'
+                        f'pk INTEGER PRIMARY KEY)')
 
     def add(self, obj: T) -> int:
         """ Добавить объект в БД """
-
         if getattr(obj, 'pk', None) != 0:
-            raise ValueError(f'trying to add object {obj} with filled `pk` attribute')
+            raise ValueError(f'trying to add object {obj}'
+                             f' with filled `pk` attribute')
         names = ', '.join(self.fields.keys())
         p = ', '.join("?" * len(self.fields))
         values = [getattr(obj, x) for x in self.fields]
         with sqlite3.connect(self.db_file) as con:
             cur = con.cursor()
             cur.execute('PRAGMA foreign_keys = ON')
-            cur.execute(
-                f'INSERT INTO {self.table_name} ({names}) VALUES ({p})', values
-            )
-            con.commit()
+            cur.execute(f'INSERT INTO '
+                        f'{self.table_name} ({names})'
+                        f'VALUES({p})', values)
             obj.pk = cur.lastrowid
         con.close()
         return obj.pk
-
-    def convert_object_datetime(self, final: list | tuple) -> tuple:
-        obj = self.cls(*final)
-        converted_temp = tuple()
-        for i in range(len(final)):
-            try:
-                converted_temp += (list(obj.__annotations__.values())[i](final[i]),)
-            except TypeError:
-                if type(final[i]) == "<class 'datetime.datetime>'":
-                    converted_temp += (list(obj.__annotations__.values(
-                    ))[i].strptime(final[i], '%Y-%m-%d %H:%M:%S'),)
-                elif final[i] is None:
-                    converted_temp += (final[i],)
-                else:
-                    converted_temp += (type(final[i])(final[i]),)
-        return converted_temp
 
     def get(self, pk: int) -> T | None:
         """ Получить объект по id """
         with sqlite3.connect(self.db_file) as con:
             cur = con.cursor()
-            res = cur.execute(f'SELECT * FROM {self.table_name} WHERE pk = {pk}')
-            final = res.fetchone()
+            cur.execute(f'SELECT * FROM {self.table_name} WHERE rowid = {pk}')
+            res = obj_make(self.cls, self.fields, cur.fetchone())
         con.close()
-        if final is None:
-            return None
-        return self.cls(*self.convert_object_datetime(final))
+        return res
 
     def get_all(self, where: dict[str, any] | None = None) -> list[T]:
         """
@@ -80,23 +64,27 @@ class SQLiteRepository(AbstractRepository[T]):
         """
         with sqlite3.connect(self.db_file) as con:
             cur = con.cursor()
-            res = cur.execute(f'SELECT * FROM {self.table_name}')
             if where is None:
-                return [self.cls(*self.convert_object_datetime(temp))
-                        for temp in res.fetchall()]
-            objs = []
-            for temp in res.fetchall():
-                obj = self.cls(*self.convert_object_datetime(temp))
-                if all([getattr(obj, attr) == value for attr, value in where.items()]):
-                    objs.append(obj)
+                cur.execute(f'SELECT * FROM {self.table_name}')
+            else:
+                p = " AND ".join(list(where.keys()))
+                values = list(where.values())
+                cur.execute(f"SELECT * FROM "
+                            f"{self.table_name} "
+                            f"WHERE {p}", values)
+            res = [obj_make(self.cls, self.fields, val) for val in cur.fetchall()]
         con.close()
-        return objs
+        return res
 
-    def update(self, obj: T) -> None:
+    def update(self, obj: T) -> T:
         """ Обновить данные об объекте. Объект должен содержать поле pk. """
-
-
-        pass
+        if obj.pk == 0:
+            raise ValueError(f'There is no object with {obj.pk} pk.')
+        fields_str = ', '.join([f"{field}=?" for field in self.fields])
+        self.cursor.execute(f"UPDATE {self.table_name} SET {fields_str} WHERE pk=?",
+                            [getattr(obj, f) for f in self.fields] + [getattr(obj, 'pk')])
+        self.conn.commit()
+        return obj
 
     def delete(self, pk: int) -> None:
         """ Удалить запись """
@@ -104,17 +92,12 @@ class SQLiteRepository(AbstractRepository[T]):
             if not self.check_pk(con.cursor(), pk):
                 raise KeyError(f'No object with id={pk}.')
             cur = con.cursor()
-            cur.execute(f'DELETE FROM {self.table_name} WHERE id = {pk}') \
+            cur.execute(f'DELETE FROM {self.table_name} WHERE rowid = {pk}')
         con.close()
 
+    def check_pk(self, cur: Any, pk: int) -> bool:
+        """ Проверка на существование записей с существующим pk.
+            Возвратит True если запись существует, и False если нет"""
 
-def check_pk(self, cur: Any, pk: int) -> bool:
-    """ Проверка на существование записей с существующим pk.
-        Возвратит True если запись существует, и False если нет"""
-
-    query = f'SELECT * FROM {self.table_name} WHERE id = {pk}'
-    res = cur.execute(query).fetchone()
-    return res is not None
-
-#         if not self.is_pk_in_db(con.cursor(), obj.pk):
-#             raise ValueError(f'trying to add object id={obj.pk} in DB.')
+        res = cur.execute(f'SELECT * FROM {self.table_name} WHERE rowid = {pk}').fetchone()
+        return res is not None
